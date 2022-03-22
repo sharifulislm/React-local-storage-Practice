bd947125f57aaf2c54f0835140f36dab1c378f6b
sharifulislm <shariful.islam.coder@gmail.com>
2022-03-22 23:20:01 +0300

    LocalStorage practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { add, multiply } from './Export/Practce/Practce';
+
+
+import Cosmetics from './components/Cosmetics/Cosmetics';
+import Shoes from './components/Shoes/Shoes';
+
+// import add from './Export/Practce/Practce';
 
 function App() {
+  const num1 = 10;
+  const num2 = 20;
+  const total = add(num1,num2)
+  const multiplys = multiply(num1,num2);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+<h1> total : {total} </h1>
+<h1> Multiply  : {multiplys} </h1>
+<Cosmetics></Cosmetics>
+      <Shoes></Shoes>
+
     </div>
   );
 }
---
+++ src/Export/Practce/Practce.js
@@ -0,0 +1,12 @@
+
+function add(num1,num2) {
+    const num = num1 + num2;
+    return num;
+  
+}
+// export default add;
+
+const multiply = (first , second) => {
+    return first * second;
+}
+export {add , multiply};
